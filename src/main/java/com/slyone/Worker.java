ab9bbe580a252f1302b6d1307db93759cf39219d
Sylvester <metagumi@outlook.com>
2018-08-03 22:34:23 -0400

    Version two commit

---
+++ src/main/java/com/slyone/Worker.java
@@ -0,0 +1,47 @@
+package com.slyone;
+
+import java.io.*;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.*;
+
+
+public class Worker {
+    private final static String TASK_QUEUE_NAME = "hello";
+
+    public static void main(String[] args)
+            throws java.io.IOException,
+            java.lang.InterruptedException,
+            TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        final Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+
+                System.out.println(" [x] Received '" + message + "'");
+                try {
+                    doWork(message);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                } finally {
+                    System.out.println(" [x] Done");
+                }
+            }
+        };
+        boolean autoAck = true; // acknowledgment is covered below
+        channel.basicConsume(TASK_QUEUE_NAME, autoAck, consumer);
+    }
+
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch: task.toCharArray()) {
+            if (ch == '.') Thread.sleep(1000);
+        }
+    }
+
+}
